ee24101e0ad6adee3bbc1c5e33d95a5fe6efcf32
Michael Schwarz <michael@7strings.de>
2025-04-16 20:24:00 +0200

    Test handover of var

--- src/mybuildtool/Program.cs
+++ src/mybuildtool/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Collections;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("My Build Tool!");
 
 Console.WriteLine();
 Console.WriteLine("Args");
@@ -11,36 +10,56 @@ foreach (var arg in args)
     Console.WriteLine($" - {arg}");
 }
 
-Console.WriteLine();
-Console.WriteLine("Environment Vars (Current Process)");
 
-IDictionary dict = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
-foreach (var key in dict.Keys)
+if (string.Equals(Environment.GetEnvironmentVariable("$GITHUB_ACTIONS"), "true", StringComparison.InvariantCultureIgnoreCase))
 {
-    string val = dict[key].ToString();
-    Console.WriteLine($" {key} = {val}");
+    Console.WriteLine("Running in a GitHub Actions environment!");
+}
+else
+{
+    Console.WriteLine("Not running in GitHub Actions.");
 }
 
 
-Console.WriteLine();
-Console.WriteLine("Environment Vars (User)");
+// Console.WriteLine();
+// Console.WriteLine("Environment Vars (Current Process)");
 
-dict = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User);
-foreach (var key in dict.Keys)
+// IDictionary dict = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
+// foreach (var key in dict.Keys)
+// {
+//     string val = dict[key].ToString();
+//     Console.WriteLine($" {key} = {val}");
+// }
+
+// Get the path to the GITHUB_ENV file from the environment variable
+var githubEnvPath = Environment.GetEnvironmentVariable("GITHUB_ENV");
+var myVariable = Environment.GetEnvironmentVariable("MY_VARIABLE");
+
+// Check if custom var exists
+if (!string.IsNullOrEmpty(myVariable))
 {
-    string val = dict[key].ToString();
-    Console.WriteLine($" {key} = {val}");
+    Console.WriteLine($"Value: {myVariable}");
 }
-
-
-Console.WriteLine();
-Console.WriteLine("Environment Vars (Machine)");
-
-dict = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine);
-foreach (var key in dict.Keys)
+// Check if the path exists
+else if (!string.IsNullOrEmpty(githubEnvPath))
 {
-    string val = dict[key].ToString();
-    Console.WriteLine($" {key} = {val}");
-}
+    try
+    {
+        string value = $"Hello from C# ({DateTime.Now:U})";
+        // Write the variable to the GITHUB_ENV file
+        using (var writer = new StreamWriter(githubEnvPath, true))
+        {
+            writer.WriteLine($"MY_VARIABLE={value}");
+        }
 
-Console.WriteLine();
+        Console.WriteLine($"Environment variable successfully set! Value={value}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred while writing to GITHUB_ENV: {ex.Message}");
+    }
+}
+else
+{
+    Console.WriteLine("GITHUB_ENV and MY_VARIABLE environment variable not found.");
+}
