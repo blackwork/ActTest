05129b7e255cb37735dbb8f7dc1522f3ec186c5b
Michael Schwarz <michael@7strings.de>
2025-04-16 20:59:25 +0200

    GitLab and TeamCity support (untested)

--- src/mybuildtool/Program.cs
+++ src/mybuildtool/Program.cs
@@ -10,17 +10,6 @@ foreach (var arg in args)
     Console.WriteLine($" - {arg}");
 }
 
-
-if (string.Equals(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"), "true", StringComparison.InvariantCultureIgnoreCase))
-{
-    Console.WriteLine("Running in a GitHub Actions environment!");
-}
-else
-{
-    Console.WriteLine("Not running in GitHub Actions.");
-}
-
-
 // Console.WriteLine();
 // Console.WriteLine("Environment Vars (Current Process)");
 
@@ -31,35 +20,98 @@ else
 //     Console.WriteLine($" {key} = {val}");
 // }
 
-// Get the path to the GITHUB_ENV file from the environment variable
-var githubEnvPath = Environment.GetEnvironmentVariable("GITHUB_ENV");
-var myVariable = Environment.GetEnvironmentVariable("MY_VARIABLE");
 
-// Check if custom var exists
-if (!string.IsNullOrEmpty(myVariable))
+
+if (string.Equals(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"), "true", StringComparison.InvariantCultureIgnoreCase))
 {
-    Console.WriteLine($"MY_VARIABLE Value: {myVariable}");
+    // Get the path to the GITHUB_ENV file from the environment variable
+    var githubEnvPath = Environment.GetEnvironmentVariable("GITHUB_ENV");
+    var myVariable = Environment.GetEnvironmentVariable("MY_VARIABLE");
+
+    // Check if custom var exists
+    if (!string.IsNullOrEmpty(myVariable))
+    {
+        Console.WriteLine($"MY_VARIABLE Value: {myVariable}");
+    }
+    // Check if the path exists
+    else if (!string.IsNullOrEmpty(githubEnvPath))
+    {
+        try
+        {
+            string value = $"Hello from C# ({DateTime.Now:U})";
+            // Write the variable to the GITHUB_ENV file
+            using (var writer = new StreamWriter(githubEnvPath, true))
+            {
+                writer.WriteLine($"MY_VARIABLE={value}");
+            }
+
+            Console.WriteLine($"Environment variable successfully set! Value={value}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while writing to GITHUB_ENV: {ex.Message}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("GITHUB_ENV and MY_VARIABLE environment variable not found.");
+    }
+
 }
-// Check if the path exists
-else if (!string.IsNullOrEmpty(githubEnvPath))
+else if (string.Equals(Environment.GetEnvironmentVariable("CI"), "true", StringComparison.InvariantCultureIgnoreCase) &&
+    Environment.GetEnvironmentVariable("GITLAB_CI") != null)
 {
-    try
+    var myVariable = Environment.GetEnvironmentVariable("MY_VARIABLE");
+
+    // Check if custom var exists
+    if (!string.IsNullOrEmpty(myVariable))
+    {
+        Console.WriteLine($"MY_VARIABLE Value: {myVariable}");
+    }
+    else
+    {
+        try
+        {
+            string value = $"Hello from C# ({DateTime.Now:U})";
+            // Write the variable to the GITHUB_ENV file
+            using (var writer = new StreamWriter(".env", true))
+            {
+                writer.WriteLine($"MY_VARIABLE={value}");
+            }
+
+            Console.WriteLine($"Environment variable successfully set! Value={value}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while writing to GitLab .env: {ex.Message}");
+        }
+    }
+    // else
+    // {
+    //     Console.WriteLine("GITHUB_ENV and MY_VARIABLE environment variable not found.");
+    // }
+
+}
+else if (string.Equals(Environment.GetEnvironmentVariable("TC"), "true", StringComparison.InvariantCultureIgnoreCase) &&
+    Environment.GetEnvironmentVariable("GITLAB_CI") != null)
+{
+    var myVariable = Environment.GetEnvironmentVariable("MY_VARIABLE");
+
+    // Check if custom var exists
+    if (!string.IsNullOrEmpty(myVariable))
+    {
+        Console.WriteLine($"MY_VARIABLE Value: {myVariable}");
+    }
+    else
     {
         string value = $"Hello from C# ({DateTime.Now:U})";
-        // Write the variable to the GITHUB_ENV file
-        using (var writer = new StreamWriter(githubEnvPath, true))
-        {
-            writer.WriteLine($"MY_VARIABLE={value}");
-        }
-
+        
+        Console.WriteLine($"##teamcity[setParameter name='env.MY_VARIABLE' value='{value}']");
         Console.WriteLine($"Environment variable successfully set! Value={value}");
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"An error occurred while writing to GITHUB_ENV: {ex.Message}");
-    }
+
 }
 else
 {
-    Console.WriteLine("GITHUB_ENV and MY_VARIABLE environment variable not found.");
+    Console.WriteLine("Not running in GitHub or GitLab Actions.");
 }
